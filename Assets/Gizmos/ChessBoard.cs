3a9cafec2b86c4434730912c253e1a302a126c7d
Rei Reizo <nathan.irwin2021@gmail.com>
2024-08-28 15:08:26 -0400

    8/28_ProgressBoard

---
+++ Assets/Gizmos/ChessBoard.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessBoard : MonoBehaviour
+{
+    private bool isWhite;
+    #if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        // Set the gizmo color
+        Gizmos.color = Color.white;
+        isWhite = true;
+
+        Vector3 gizmoPosition = transform.position;
+
+        for (int i = 0; i < 8; i++)
+        {
+            gizmoPosition.y = i;
+            for(int j = 0;  j < 8; j++)
+            {
+                gizmoPosition.x = j;
+                Gizmos.DrawCube(gizmoPosition, new Vector3(1, 1, 1));
+
+                if (isWhite)
+                {
+                    Gizmos.color = Color.black;
+                    isWhite = false;
+                }
+
+                else
+                {
+                    Gizmos.color = Color.white;
+                    isWhite = true;
+                }
+            }
+            if (isWhite)
+            {
+                Gizmos.color = Color.black;
+                isWhite = false;
+            }
+
+            else
+            {
+                Gizmos.color = Color.white;
+                isWhite = true;
+            }
+        }
+    }
+    #endif
+}
